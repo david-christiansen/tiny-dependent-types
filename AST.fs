10e0a0bf100428d3950473229d49b0720c34e72c
David Christiansen <drc@itu.dk>
2012-08-03 11:14:24 +0200

    Use syntactic sugar for useless binders in pretty-printer.
    Now, degenerate pi-types show up as arrows, degenerate sigmas as products, and
    degenerate dependent pairs as ordinary pairs.

--- AST.fs
+++ AST.fs
@@ -79,17 +79,46 @@ let subscriptStringOf (str : string) =
      .Replace("8", "₈")
      .Replace("9", "₉")
 
+let rec usesBinding n t =
+  match t with
+    | Bound n' when n = n' -> true
+    | Bound _ -> false
+    | Free _ -> false
+    | Pi (_, t1, t2) -> usesBinding n t1 || usesBinding (S n) t2
+    | Lambda (_, t1, t2) -> usesBinding n t1 || usesBinding (S n) t2
+    | Sigma (_, t1, t2) -> usesBinding n t1 || usesBinding (S n) t2
+    | Pair (_, t1, t2) -> usesBinding n t1 || usesBinding (S n) t2
+    | Fst t' -> usesBinding n t'
+    | Snd t' -> usesBinding n t'
+    | App (t1, t2) -> usesBinding n t1 || usesBinding n t2
+    | Univ _ -> false
+    | Postulated _ -> false
+    | Datatype _ -> false
+    | Constructor (_, args) ->
+      let rec bindingIn n = function
+        | [] -> false
+        | ar :: ars -> usesBinding n ar || bindingIn (S n) ars
+      bindingIn n args
+
 let rec pprintTerm t = pprintTerm' t []
 and pprintTerm' t ctx =
   match t with
     | Bound n -> "%" + ctx.Item(intOfNat n) + "%" + pprintNat n
     | Free str -> "`" + str + "`"
+    | Pi (x, t1, t2) when not (usesBinding Z t2) ->
+        "(" + pprintTerm' t1 ctx +
+        " → " + pprintTerm' t2 (""::ctx) + ")"
     | Pi (x, t1, t2) -> "Π" + x + ":" + pprintTerm' t1 ctx +
                         "." + pprintTerm' t2 (x::ctx)
     | Lambda (x, t1, t2) -> "λ" + x + ":" + pprintTerm' t1 ctx +
                             "." + pprintTerm' t2 (x::ctx)
+    | Sigma (_, ty, p) when not (usesBinding Z p) ->
+        "(" + pprintTerm' ty ctx +
+        " × " + pprintTerm' p (""::ctx) + ")"
     | Sigma (x, ty, p) -> "Σ" + x + ":" + pprintTerm' ty ctx +
                           "." + pprintTerm' p (x::ctx)
+    | Pair (_, a, b) when not (usesBinding Z b) ->
+        "{" + pprintTerm' a ctx + " ; " + pprintTerm' b (""::ctx) + "}"
     | Pair (x, w, prf) -> "{" + x + "|" +
                           pprintTerm' w ctx + ";" +
                           pprintTerm' prf (x::ctx) + "}"
